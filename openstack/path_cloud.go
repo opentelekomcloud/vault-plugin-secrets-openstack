7ee856c5a9c3c5fbaf8cb008c4025987c598fc58
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2022-02-15 17:10:38 +0300

    Implement `create`/`update` `cloud` path (#19)
    Implement `create`/`update` `cloud` path
    
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>
    Reviewed-by: Anton Sidelnikov <None>

--- openstack/path_cloud.go
+++ openstack/path_cloud.go
@@ -4,10 +4,18 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
 )
 
-const pathCloud = "cloud"
+const (
+	pathCloud = "cloud"
+
+	pathCloudHelpSyn = `Configure the root credentials for an OpenStack cloud.`
+	pathCloudHelpDes = `
+Configure the root credentials for an OpenStack cloud using the above parameters.
+`
+)
 
 func cloudKey(name string) string {
 	return fmt.Sprintf("%s/%s", pathCloud, name)
@@ -26,4 +34,95 @@ func (c *sharedCloud) getCloudConfig(ctx context.Context, s logical.Storage) (*O
 		return nil, err
 	}
 	return cloud, nil
+}
+
+func (cloud *OsCloud) save(ctx context.Context, s logical.Storage) error {
+	entry, err := logical.StorageEntryJSON(cloudKey(cloud.Name), cloud)
+	if err != nil {
+		return err
+	}
+
+	return s.Put(ctx, entry)
+}
+
+func (b *backend) pathCloud() *framework.Path {
+	return &framework.Path{
+		Pattern: fmt.Sprintf("%s/%s", pathCloud, framework.GenericNameWithAtRegex("name")),
+		Fields: map[string]*framework.FieldSchema{
+			"name": {
+				Type:        framework.TypeLowerCaseString,
+				Required:    true,
+				Description: "Name of the cloud.",
+			},
+			"auth_url": {
+				Type:        framework.TypeString,
+				Required:    true,
+				Description: "URL of identity service authentication endpoint.",
+			},
+			"user_domain_name": {
+				Type:        framework.TypeString,
+				Required:    true,
+				Description: "Name of the domain of the root user.",
+			},
+			"username": {
+				Type:        framework.TypeString,
+				Required:    true,
+				Description: "OpenStack username of the root user.",
+			},
+			"password": {
+				Type:        framework.TypeString,
+				Required:    true,
+				Description: "OpenStack password of the root user.",
+				DisplayAttrs: &framework.DisplayAttributes{
+					Sensitive: true,
+				},
+			},
+		},
+		Operations: map[logical.Operation]framework.OperationHandler{
+			logical.CreateOperation: &framework.PathOperation{
+				Callback: b.pathCloudCreateUpdate,
+			},
+			logical.UpdateOperation: &framework.PathOperation{
+				Callback: b.pathCloudCreateUpdate,
+			},
+		},
+		HelpSynopsis:    pathCloudHelpSyn,
+		HelpDescription: pathCloudHelpDes,
+	}
+}
+
+func (b *backend) pathCloudCreateUpdate(ctx context.Context, r *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+	name := d.Get("name").(string)
+
+	sCloud := b.getSharedCloud(name)
+
+	cloudConfig, err := sCloud.getCloudConfig(ctx, r.Storage)
+	if err != nil {
+		return nil, err
+	}
+
+	if cloudConfig == nil {
+		cloudConfig = &OsCloud{
+			Name: name,
+		}
+	}
+
+	if authURL, ok := d.GetOk("auth_url"); ok {
+		cloudConfig.AuthURL = authURL.(string)
+	}
+	if userDomainName, ok := d.GetOk("user_domain_name"); ok {
+		cloudConfig.UserDomainName = userDomainName.(string)
+	}
+	if username, ok := d.GetOk("username"); ok {
+		cloudConfig.Username = username.(string)
+	}
+	if password, ok := d.GetOk("password"); ok {
+		cloudConfig.Password = password.(string)
+	}
+
+	if err := cloudConfig.save(ctx, r.Storage); err != nil {
+		return logical.ErrorResponse(err.Error()), nil
+	}
+
+	return nil, nil
 }
--- openstack/path_cloud_test.go
+++ openstack/path_cloud_test.go
@@ -2,21 +2,140 @@ package openstack
 
 import (
 	"context"
-	"sync"
+	"strings"
 	"testing"
 
-	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/acceptance/tools"
+	th "github.com/gophercloud/gophercloud/testhelper"
+	thClient "github.com/gophercloud/gophercloud/testhelper/client"
+	"github.com/hashicorp/vault/sdk/logical"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSharedCloud_getCloudConfig(t *testing.T) {
-	// TODO: this test must be implemented after implementing getCloudConfig
-	cloud := &sharedCloud{
-		client: new(gophercloud.ServiceClient),
-		lock:   sync.Mutex{},
-	}
-	_, s := testBackend(t)
+var (
+	testCloudName      = strings.ToLower(tools.RandomString("cloud", 3))
+	testAuthURL        = tools.RandomString("https://test.com/", 3)
+	testUsername       = tools.RandomString("user", 3)
+	testUserDomainName = tools.RandomString("domain", 3)
+	testPassword1      = tools.RandomString("password1", 3)
+	testPassword2      = tools.RandomString("password2", 3)
+)
 
-	_, err := cloud.getCloudConfig(context.Background(), s)
-	assert.NoError(t, err)
+func TestCloudCreate(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	testClient := thClient.ServiceClient()
+
+	t.Run("Success", func(t *testing.T) {
+		b, storage := testBackend(t)
+
+		authURL := testClient.Endpoint + "v3"
+
+		entry, err := logical.StorageEntryJSON(cloudKey(testCloudName), &OsCloud{
+			Name:           testCloudName,
+			AuthURL:        authURL,
+			UserDomainName: testUserDomainName,
+			Username:       testUsername,
+			Password:       testPassword1,
+		})
+		assert.NoError(t, err)
+		assert.NoError(t, storage.Put(context.Background(), entry))
+
+		res, err := b.HandleRequest(context.Background(), &logical.Request{
+			Storage:   storage,
+			Operation: logical.CreateOperation,
+			Path:      cloudKey(testCloudName),
+		})
+		assert.NoError(t, err)
+		assert.Empty(t, res)
+	})
+
+	t.Run("EmptyConfig", func(t *testing.T) {
+		b, storage := testBackend(t)
+
+		res, err := b.HandleRequest(context.Background(), &logical.Request{
+			Storage:   storage,
+			Operation: logical.CreateOperation,
+			Path:      cloudKey(testCloudName),
+		})
+		assert.NoError(t, err)
+		assert.Empty(t, res)
+	})
+
+	t.Run("FullConfig", func(t *testing.T) {
+		b, storage := testBackend(t)
+
+		_, err := b.HandleRequest(context.Background(), &logical.Request{
+			Storage:   storage,
+			Operation: logical.CreateOperation,
+			Path:      cloudKey(testCloudName),
+			Data: map[string]interface{}{
+				"name":             testCloudName,
+				"auth_url":         testAuthURL,
+				"user_domain_name": testUserDomainName,
+				"username":         testUsername,
+				"password":         testPassword1,
+			},
+		})
+		assert.NoError(t, err)
+
+		sCloud := b.getSharedCloud(testCloudName)
+		cloudConfig, err := sCloud.getCloudConfig(context.Background(), storage)
+		assert.NoError(t, err)
+		assert.Equal(t, cloudConfig.AuthURL, testAuthURL)
+		assert.Equal(t, cloudConfig.UserDomainName, testUserDomainName)
+		assert.Equal(t, cloudConfig.Username, testUsername)
+		assert.Equal(t, cloudConfig.Password, testPassword1)
+		assert.Equal(t, cloudConfig.Name, testCloudName)
+	})
+
+	t.Run("FullConfigUpdate", func(t *testing.T) {
+		b, storage := testBackend(t)
+
+		_, err := b.HandleRequest(context.Background(), &logical.Request{
+			Storage:   storage,
+			Operation: logical.CreateOperation,
+			Path:      cloudKey(testCloudName),
+			Data: map[string]interface{}{
+				"name":             testCloudName,
+				"auth_url":         testAuthURL,
+				"user_domain_name": testUserDomainName,
+				"username":         testUsername,
+				"password":         testPassword1,
+			},
+		})
+		assert.NoError(t, err)
+
+		sCloud := b.getSharedCloud(testCloudName)
+		cloudConfig, err := sCloud.getCloudConfig(context.Background(), storage)
+		assert.NoError(t, err)
+		assert.Equal(t, cloudConfig.AuthURL, testAuthURL)
+		assert.Equal(t, cloudConfig.UserDomainName, testUserDomainName)
+		assert.Equal(t, cloudConfig.Username, testUsername)
+		assert.Equal(t, cloudConfig.Password, testPassword1)
+		assert.Equal(t, cloudConfig.Name, testCloudName)
+
+		_, err = b.HandleRequest(context.Background(), &logical.Request{
+			Storage:   storage,
+			Operation: logical.UpdateOperation,
+			Path:      cloudKey(testCloudName),
+			Data: map[string]interface{}{
+				"name":             testCloudName,
+				"auth_url":         testAuthURL,
+				"user_domain_name": testUserDomainName,
+				"username":         testUsername,
+				"password":         testPassword2,
+			},
+		})
+		assert.NoError(t, err)
+
+		cloudConfig, err = sCloud.getCloudConfig(context.Background(), storage)
+		assert.NoError(t, err)
+		assert.Equal(t, cloudConfig.AuthURL, testAuthURL)
+		assert.Equal(t, cloudConfig.UserDomainName, testUserDomainName)
+		assert.Equal(t, cloudConfig.Username, testUsername)
+		assert.Equal(t, cloudConfig.Password, testPassword2)
+		assert.Equal(t, cloudConfig.Name, testCloudName)
+	})
 }
