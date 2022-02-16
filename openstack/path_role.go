591784b05f3175d8db9ca88e73d67d886ff79cc5
Anton Kachurin <anton.kachurin@t-systems.com>
2022-02-16 19:37:38 +0300

    Implement operations for the roles (#21)
    Implement operations for the roles
    
    Add role endpoint for role CRD operations
    Add roles endpoint for listing existing roles
    Resolve #15
    Resolve #16
    
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>

---
+++ openstack/path_role.go
@@ -0,0 +1,322 @@
+package openstack
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/logical"
+)
+
+const (
+	rolesStoragePath = "roles"
+	pathRoles        = `roles/?`
+
+	errInvalidForRoot = "impossible to set %s for the root user"
+
+	rolesListHelpSyn  = `List existing roles.`
+	rolesListHelpDesc = `
+List existing roles by name. Supports filtering by cloud.
+`
+	roleHelpSyn  = "Manage the Vault roles used to generate OpenStack credentials."
+	roleHelpDesc = `
+This path allows you to read and write roles that are used to generate OpenStack login
+credentials. These roles are associated with either an existing user, or a list of user groups,
+which are used to control permissions to OpenStack resources.
+`
+)
+
+var (
+	pathRole = fmt.Sprintf("role/%s", framework.GenericNameRegex("name"))
+
+	errRoleGet = errors.New("error searching for the role")
+)
+
+func (b *backend) pathRoles() *framework.Path {
+	return &framework.Path{
+		Pattern: pathRoles,
+		Fields: map[string]*framework.FieldSchema{
+			"cloud": {
+				Type:        framework.TypeNameString,
+				Description: "Specifies root configuration of the created role.",
+				Required:    true,
+			},
+		},
+		Operations: map[logical.Operation]framework.OperationHandler{
+			logical.ListOperation: &framework.PathOperation{
+				Callback: b.pathRolesList,
+			},
+		},
+		HelpSynopsis:    rolesListHelpSyn,
+		HelpDescription: rolesListHelpDesc,
+	}
+}
+
+func (b *backend) pathRole() *framework.Path {
+	return &framework.Path{
+		Pattern: pathRole,
+		Fields: map[string]*framework.FieldSchema{
+			"name": {
+				Type:        framework.TypeString,
+				Description: "Specifies the name of the role to create. This is part of the request URL.",
+			},
+			"cloud": {
+				Type:        framework.TypeString,
+				Description: "Specifies root configuration of the created role.",
+			},
+			"root": {
+				Type:        framework.TypeBool,
+				Description: "Specifies whenever to use the root user as a role actor.",
+				Default:     false,
+			},
+			"ttl": {
+				Type:        framework.TypeDurationSecond,
+				Description: "Specifies TTL value for the dynamically created users as a string duration with time suffix.",
+				Default:     "1h",
+			},
+			"secret_type": {
+				Type:          framework.TypeLowerCaseString,
+				Description:   "Specifies what kind of secret will configuration contain.",
+				AllowedValues: []interface{}{"token", "password"},
+				Default:       "token",
+			},
+			"user_groups": {
+				Type:        framework.TypeCommaStringSlice,
+				Description: "Specifies list of existing OpenStack groups this Vault role is allowed to assume.",
+			},
+			"project_id": {
+				Type:        framework.TypeLowerCaseString,
+				Description: "Specifies a project ID for project-scoped role.",
+			},
+			"project_name": {
+				Type:        framework.TypeNameString,
+				Description: "Specifies a project ID for project-scoped role.",
+			},
+			"extensions": {
+				Type: framework.TypeKVPairs,
+				Description: "A list of strings representing a key/value pair to be used as extensions to the cloud " +
+					"configuration (e.g. `volume_api_version` or endpoint overrides).",
+			},
+		},
+		Operations: map[logical.Operation]framework.OperationHandler{
+			logical.ReadOperation: &framework.PathOperation{
+				Callback: b.pathRoleRead,
+			},
+			logical.CreateOperation: &framework.PathOperation{
+				Callback: b.pathRoleUpdate,
+			},
+			logical.UpdateOperation: &framework.PathOperation{
+				Callback: b.pathRoleUpdate,
+			},
+			logical.DeleteOperation: &framework.PathOperation{
+				Callback: b.pathRoleDelete,
+			},
+		},
+		ExistenceCheck:  b.roleExistenceCheck,
+		HelpSynopsis:    roleHelpSyn,
+		HelpDescription: roleHelpDesc,
+	}
+}
+
+func (b *backend) roleExistenceCheck(ctx context.Context, r *logical.Request, d *framework.FieldData) (bool, error) {
+	role, err := getRole(ctx, d, r.Storage)
+	if err != nil {
+		return false, err
+	}
+	return role != nil, nil
+}
+
+type secretType string
+
+const (
+	SecretPassword secretType = "password"
+	SecretToken    secretType = "token"
+)
+
+type roleEntry struct {
+	Cloud       string            `json:"cloud,omitempty"`
+	Root        bool              `json:"root"`
+	TTL         time.Duration     `json:"ttl,omitempty"`
+	SecretType  secretType        `json:"secret_type"`
+	UserGroups  []string          `json:"user_groups"`
+	ProjectID   string            `json:"project_id"`
+	ProjectName string            `json:"project_name"`
+	Extensions  map[string]string `json:"extensions"`
+}
+
+func roleStoragePath(name string) string {
+	return fmt.Sprintf("%s/%s", rolesStoragePath, name)
+}
+
+func getRole(ctx context.Context, d *framework.FieldData, s logical.Storage) (*roleEntry, error) {
+	name := d.Get("name").(string)
+	return getRoleByName(ctx, name, s)
+}
+
+func saveRole(ctx context.Context, name string, e *roleEntry, s logical.Storage) error {
+	storageEntry, err := logical.StorageEntryJSON(roleStoragePath(name), e)
+	if err != nil {
+		return err
+	}
+	return s.Put(ctx, storageEntry)
+}
+
+func getRoleByName(ctx context.Context, name string, s logical.Storage) (*roleEntry, error) {
+	entry, err := s.Get(ctx, roleStoragePath(name))
+	if err != nil {
+		return nil, err
+	}
+
+	if entry == nil {
+		return nil, nil
+	}
+
+	role := new(roleEntry)
+	if err := entry.DecodeJSON(role); err != nil {
+		return nil, err
+	}
+	return role, nil
+}
+
+func roleToMap(src *roleEntry) map[string]interface{} {
+	return map[string]interface{}{
+		"cloud":        src.Cloud,
+		"root":         src.Root,
+		"ttl":          src.TTL,
+		"secret_type":  string(src.SecretType),
+		"user_groups":  src.UserGroups,
+		"project_id":   src.ProjectID,
+		"project_name": src.ProjectName,
+		"extensions":   src.Extensions,
+	}
+}
+
+func (b *backend) pathRoleRead(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+	entry, err := getRole(ctx, d, req.Storage)
+	if err != nil {
+		return nil, errRoleGet
+	}
+	if entry == nil {
+		return logical.ErrorResponse("role not found"), nil
+	}
+
+	data := roleToMap(entry)
+	return &logical.Response{
+		Data: data,
+	}, nil
+}
+
+func (b *backend) pathRoleUpdate(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+	name := d.Get("name").(string)
+
+	entry, err := getRoleByName(ctx, name, req.Storage)
+	if err != nil {
+		return logical.ErrorResponse("failed to find the role"), err
+	}
+	if entry == nil {
+		if req.Operation == logical.UpdateOperation {
+			return logical.ErrorResponse("role `%s` not found during update operation", name), nil
+		}
+		entry = new(roleEntry)
+	}
+
+	if cloud, ok := d.GetOk("cloud"); ok {
+		entry.Cloud = cloud.(string)
+	} else {
+		if req.Operation == logical.CreateOperation {
+			return logical.ErrorResponse("cloud is required when creating a role"), nil
+		}
+	}
+
+	if isRoot, ok := d.GetOk("root"); ok {
+		entry.Root = isRoot.(bool)
+	}
+
+	if !entry.Root {
+		if ttl, ok := d.GetOk("ttl"); ok {
+			entry.TTL = time.Duration(ttl.(int)) * time.Second
+		} else if req.Operation == logical.CreateOperation {
+			entry.TTL = time.Hour
+		}
+	} else {
+		if _, ok := d.GetOk("ttl"); ok {
+			return logical.ErrorResponse(errInvalidForRoot, "ttl"), nil
+		}
+	}
+
+	if typ, ok := d.GetOk("secret_type"); ok {
+		if entry.Root {
+			return logical.ErrorResponse(errInvalidForRoot, "secret type"), nil
+		}
+		entry.SecretType = secretType(typ.(string))
+	} else if req.Operation == logical.CreateOperation {
+		entry.SecretType = SecretToken
+	}
+
+	if name, ok := d.GetOk("project_name"); ok {
+		entry.ProjectName = name.(string)
+	}
+
+	if id, ok := d.GetOk("project_id"); ok {
+		entry.ProjectID = id.(string)
+	}
+
+	if ext, ok := d.GetOk("extensions"); ok {
+		entry.Extensions = ext.(map[string]string)
+	}
+
+	if groups, ok := d.GetOk("user_groups"); ok {
+		if entry.Root {
+			return logical.ErrorResponse(errInvalidForRoot, "user groups"), nil
+		}
+		entry.UserGroups = groups.([]string)
+	}
+
+	if err := saveRole(ctx, name, entry, req.Storage); err != nil {
+		return nil, err
+	}
+
+	return &logical.Response{}, nil
+}
+
+func (b *backend) pathRoleDelete(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+	name := d.Get("name").(string)
+	entry, err := req.Storage.Get(ctx, roleStoragePath(name))
+	if err != nil {
+		return nil, err
+	}
+
+	if entry == nil {
+		return &logical.Response{}, nil
+	}
+
+	err = req.Storage.Delete(ctx, roleStoragePath(name))
+	return &logical.Response{}, err
+}
+
+func (b *backend) pathRolesList(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+	roles, err := req.Storage.List(ctx, rolesStoragePath+"/")
+	if err != nil {
+		return nil, err
+	}
+
+	// filter by cloud
+	if cloud, ok := d.GetOk("cloud"); ok {
+		var refinedRoles []string
+		for _, name := range roles {
+			role, err := getRoleByName(ctx, name, req.Storage)
+			if err != nil {
+				return nil, err
+			}
+			if role.Cloud != cloud {
+				continue
+			}
+			refinedRoles = append(refinedRoles, name)
+		}
+		roles = refinedRoles
+	}
+
+	return logical.ListResponse(roles), nil
+}
---
+++ openstack/path_role_test.go
@@ -0,0 +1,523 @@
+package openstack
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/gophercloud/gophercloud/acceptance/tools"
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/opentelekomcloud/vault-plugin-secrets-openstack/openstack/fixtures"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func rolePath(name string) string {
+	return fmt.Sprintf("%s/%s", "role", name)
+}
+
+func TestRoleStoragePath(t *testing.T) {
+	name := tools.RandomString("role", 5)
+	expected := "roles/" + name
+	actual := roleStoragePath(name)
+	assert.Equal(t, actual, expected)
+}
+
+func randomRoleName() string {
+	return tools.RandomString("k", 5) + "m"
+}
+
+func expectedRoleData() (*roleEntry, map[string]interface{}) {
+	expTTL := time.Hour
+	expected := &roleEntry{
+		Cloud:       tools.RandomString("cl", 5),
+		TTL:         expTTL / time.Second,
+		ProjectName: tools.RandomString("p", 5),
+	}
+	expectedMap := map[string]interface{}{
+		"cloud":        expected.Cloud,
+		"ttl":          expTTL / time.Second,
+		"project_id":   "",
+		"project_name": expected.ProjectName,
+		"extensions":   map[string]string{},
+		"root":         false,
+		"secret_type":  "token",
+		"user_groups":  []string{},
+	}
+	return expected, expectedMap
+}
+
+func saveRawRole(t *testing.T, name string, raw map[string]interface{}, s logical.Storage) {
+	storeEntry, err := logical.StorageEntryJSON(roleStoragePath(name), raw)
+	require.NoError(t, err)
+	require.NoError(t, s.Put(context.Background(), storeEntry))
+}
+
+func TestRoleGet(t *testing.T) {
+	t.Parallel()
+
+	t.Run("existing", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t)
+
+		roleName := randomRoleName()
+		_, expectedMap := expectedRoleData()
+
+		saveRawRole(t, roleName, expectedMap, s)
+
+		resp, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.ReadOperation,
+			Path:      rolePath(roleName),
+			Storage:   s,
+		})
+		require.NoError(t, err)
+		require.NotEmpty(t, resp)
+		assert.Equal(t, expectedMap, resp.Data)
+	})
+
+	t.Run("not-existing", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t)
+		roleName := tools.RandomString("k", 5)
+
+		resp, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.ReadOperation,
+			Path:      rolePath(roleName),
+			Storage:   s,
+		})
+		require.NoError(t, err)
+		assert.NotEmpty(t, resp.Data["error"])
+	})
+
+	t.Run("get-err", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t, failVerbRead)
+		roleName := tools.RandomString("k", 5)
+
+		_, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.ReadOperation,
+			Path:      rolePath(roleName),
+			Storage:   s,
+		})
+		require.Error(t, err)
+		assert.ErrorIs(t, err, errRoleGet)
+	})
+}
+
+func TestRoleExistence(t *testing.T) {
+	t.Parallel()
+
+	t.Run("existing", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t)
+
+		roleName := randomRoleName()
+		_, exp := expectedRoleData()
+		saveRawRole(t, roleName, exp, s)
+
+		req := &logical.Request{Storage: s}
+		fData := &framework.FieldData{
+			Schema: b.pathRole().Fields,
+			Raw:    map[string]interface{}{"name": roleName},
+		}
+		ok, err := b.roleExistenceCheck(context.Background(), req, fData)
+		require.NoError(t, err)
+		assert.True(t, ok)
+	})
+
+	t.Run("not-existing", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t)
+
+		roleName := randomRoleName()
+
+		req := &logical.Request{Storage: s}
+		fData := &framework.FieldData{
+			Schema: b.pathRole().Fields,
+			Raw:    map[string]interface{}{"name": roleName},
+		}
+		ok, err := b.roleExistenceCheck(context.Background(), req, fData)
+		require.NoError(t, err)
+		assert.False(t, ok)
+	})
+
+	t.Run("get-err", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t, failVerbRead)
+
+		roleName := randomRoleName()
+
+		req := &logical.Request{Storage: s}
+		fData := &framework.FieldData{
+			Schema: b.pathRole().Fields,
+			Raw:    map[string]interface{}{"name": roleName},
+		}
+		_, err := b.roleExistenceCheck(context.Background(), req, fData)
+		assert.Error(t, err, errRoleGet)
+	})
+}
+
+func TestRoleList(t *testing.T) {
+	t.Parallel()
+
+	t.Run("ok", func(t *testing.T) {
+		b, s := testBackend(t)
+		roleCount := tools.RandomInt(1, 10)
+		roleNames := make([]string, roleCount)
+
+		for i := 0; i < roleCount; i++ {
+			name := randomRoleName()
+			roleNames[i] = name
+			_, exp := expectedRoleData()
+			saveRawRole(t, name, exp, s)
+		}
+
+		lst, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.ListOperation,
+			Path:      "roles/",
+			Storage:   s,
+		})
+		require.NoError(t, err)
+		require.NotEmpty(t, lst.Data)
+		require.Len(t, lst.Data["keys"], roleCount)
+		for _, name := range roleNames {
+			assert.Contains(t, lst.Data["keys"], name)
+		}
+	})
+
+	t.Run("error", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t, failVerbList)
+		_, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.ListOperation,
+			Path:      "roles/",
+			Storage:   s,
+		})
+		require.Error(t, err)
+	})
+
+	t.Run("filter", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t)
+		name1 := randomRoleName()
+		expRole1, expMap1 := expectedRoleData()
+		saveRawRole(t, name1, expMap1, s)
+		name2 := randomRoleName()
+		_, expMap2 := expectedRoleData()
+		saveRawRole(t, name2, expMap2, s)
+
+		lst, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.ListOperation,
+			Path:      "roles/",
+			Data: map[string]interface{}{
+				"cloud": expRole1.Cloud,
+			},
+			Storage: s,
+		})
+		require.NoError(t, err)
+		assert.Len(t, lst.Data["keys"], 1)
+		assert.Equal(t, name1, lst.Data["keys"].([]string)[0])
+	})
+
+	t.Run("filter-get-err", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t, failVerbRead)
+		name1 := randomRoleName()
+		expRole1, expMap1 := expectedRoleData()
+		saveRawRole(t, name1, expMap1, s)
+
+		_, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.ListOperation,
+			Path:      "roles/",
+			Data: map[string]interface{}{
+				"cloud": expRole1.Cloud,
+			},
+			Storage: s,
+		})
+		require.Error(t, err)
+	})
+}
+
+func TestRoleDelete(t *testing.T) {
+	t.Parallel()
+
+	t.Run("existing", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t)
+
+		roleName := randomRoleName()
+		_, expectedMap := expectedRoleData()
+		saveRawRole(t, roleName, expectedMap, s)
+
+		resp, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.DeleteOperation,
+			Path:      rolePath(roleName),
+			Storage:   s,
+		})
+		require.NoError(t, err)
+		require.Empty(t, resp)
+	})
+
+	t.Run("not-existing", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t)
+		roleName := randomRoleName()
+		resp, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.DeleteOperation,
+			Path:      rolePath(roleName),
+			Storage:   s,
+		})
+		require.NoError(t, err)
+		require.Empty(t, resp)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t, failVerbDelete)
+
+		roleName := randomRoleName()
+		_, expectedMap := expectedRoleData()
+		saveRawRole(t, roleName, expectedMap, s)
+
+		_, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.DeleteOperation,
+			Path:      rolePath(roleName),
+			Storage:   s,
+		})
+		require.Error(t, err)
+	})
+
+	t.Run("error-get", func(t *testing.T) {
+		t.Parallel()
+		b, s := testBackend(t, failVerbRead)
+
+		roleName := randomRoleName()
+		_, expectedMap := expectedRoleData()
+		saveRawRole(t, roleName, expectedMap, s)
+
+		_, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.DeleteOperation,
+			Path:      rolePath(roleName),
+			Storage:   s,
+		})
+		require.Error(t, err)
+	})
+}
+
+func TestRoleCreate(t *testing.T) {
+	t.Parallel()
+
+	id, _ := uuid.GenerateUUID()
+	t.Run("ok", func(t *testing.T) {
+		cases := map[string]*roleEntry{
+			"admin": {
+				Cloud: randomRoleName(),
+				Root:  true,
+			},
+			"token": {
+				Cloud:       randomRoleName(),
+				ProjectName: randomRoleName(),
+				SecretType:  SecretToken,
+				UserGroups:  []string{"default", "testing"},
+			},
+			"password": {
+				Cloud:       randomRoleName(),
+				ProjectName: randomRoleName(),
+				SecretType:  SecretPassword,
+				UserGroups:  []string{"default", "testing"},
+			},
+			"ttl": {
+				Cloud:       randomRoleName(),
+				ProjectName: randomRoleName(),
+				SecretType:  SecretToken,
+				UserGroups:  []string{"default", "testing"},
+				TTL:         24 * time.Hour,
+			},
+			"endpoint-override": {
+				Cloud:     randomRoleName(),
+				ProjectID: id,
+				Extensions: map[string]string{
+					"volume_api_version":             "3",
+					"object_store_endpoint_override": "https://swift.example.com",
+				},
+			},
+		}
+
+		for name, data := range cases {
+			t.Run(name, func(t *testing.T) {
+				data := data
+				t.Parallel()
+				b, s := testBackend(t)
+
+				roleName := randomRoleName()
+				inputRole := fixtures.SanitizedMap(roleToMap(data))
+
+				resp, err := b.HandleRequest(context.Background(), &logical.Request{
+					Operation: logical.CreateOperation,
+					Path:      rolePath(roleName),
+					Data:      inputRole,
+					Storage:   s,
+				})
+				require.NoError(t, err)
+				require.Empty(t, resp)
+
+				entry, err := s.Get(context.Background(), roleStoragePath(roleName))
+				require.NoError(t, err)
+				require.NotEmpty(t, entry)
+				role := new(roleEntry)
+				assert.NoError(t, entry.DecodeJSON(role))
+
+				fillRoleDefaultFields(b, data) // otherwise there will be false positives
+				assert.Equal(t, data, role)
+			})
+		}
+	})
+
+	t.Run("error", func(t *testing.T) {
+		type errRoleEntry struct {
+			*roleEntry
+			errorRegex *regexp.Regexp
+		}
+
+		notForRootRe := regexp.MustCompile(`impossible to set .+ for the root user`)
+		cases := map[string]*errRoleEntry{
+			"root-ttl": {
+				roleEntry: &roleEntry{
+					Cloud: randomRoleName(),
+					Root:  true,
+					TTL:   1 * time.Hour,
+				},
+				errorRegex: notForRootRe,
+			},
+			"root-password": {
+				roleEntry: &roleEntry{
+					Cloud:      randomRoleName(),
+					Root:       true,
+					SecretType: SecretPassword,
+				},
+				errorRegex: notForRootRe,
+			},
+			"root-user-groups": {
+				roleEntry: &roleEntry{
+					Cloud:      randomRoleName(),
+					Root:       true,
+					UserGroups: []string{"ug-1"},
+				},
+				errorRegex: notForRootRe,
+			},
+			"without-cloud": {
+				roleEntry:  &roleEntry{},
+				errorRegex: regexp.MustCompile(`cloud is required when creating a role`),
+			},
+		}
+
+		for name, data := range cases {
+			t.Run(name, func(t *testing.T) {
+				data := data
+				t.Parallel()
+				b, s := testBackend(t)
+
+				roleName := randomRoleName()
+				inputRole := fixtures.SanitizedMap(roleToMap(data.roleEntry))
+
+				resp, err := b.HandleRequest(context.Background(), &logical.Request{
+					Operation: logical.CreateOperation,
+					Path:      rolePath(roleName),
+					Data:      inputRole,
+					Storage:   s,
+				})
+				require.NoError(t, err)
+				require.True(t, resp.IsError())
+				assert.Regexp(t, data.errorRegex, resp.Data["error"])
+			})
+		}
+	})
+
+	t.Run("store-err", func(t *testing.T) {
+		data := &roleEntry{
+			Cloud:       randomRoleName(),
+			ProjectName: randomRoleName(),
+			SecretType:  SecretToken,
+			UserGroups:  []string{"default", "testing"},
+		}
+		cases := map[string]failVerb{
+			"read":  failVerbRead,
+			"write": failVerbPut,
+		}
+		for name, verb := range cases {
+			t.Run(name, func(t *testing.T) {
+				b, s := testBackend(t, verb)
+				t.Parallel()
+
+				roleName := randomRoleName()
+				inputRole := fixtures.SanitizedMap(roleToMap(data))
+
+				_, err := b.HandleRequest(context.Background(), &logical.Request{
+					Operation: logical.CreateOperation,
+					Path:      rolePath(roleName),
+					Data:      inputRole,
+					Storage:   s,
+				})
+				require.Error(t, err)
+
+			})
+		}
+	})
+}
+
+func TestRoleUpdate(t *testing.T) {
+	t.Parallel()
+
+	b, s := testBackend(t)
+
+	t.Run("ok", func(t *testing.T) {
+		roleName := randomRoleName()
+		_, exp := expectedRoleData()
+		exp2 := &roleEntry{
+			ProjectID:   "",
+			ProjectName: tools.RandomString("p", 5),
+		}
+		saveRawRole(t, roleName, exp, s)
+
+		resp, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.UpdateOperation,
+			Path:      rolePath(roleName),
+			Data:      roleToMap(exp2),
+			Storage:   s,
+		})
+		require.NoError(t, err)
+		assert.False(t, resp.IsError(), resp)
+	})
+
+	t.Run("not-existing", func(t *testing.T) {
+		roleName := randomRoleName()
+		_, exp := expectedRoleData()
+
+		resp, err := b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.UpdateOperation,
+			Path:      rolePath(roleName),
+			Data:      exp,
+			Storage:   s,
+		})
+		require.NoError(t, err)
+		assert.True(t, resp.IsError())
+		assert.Regexp(t, regexp.MustCompile(`role .+ not found during update operation`), resp.Data["error"])
+	})
+}
+
+func fillRoleDefaultFields(b *backend, entry *roleEntry) {
+	pr := b.pathRole()
+	flds := pr.Fields
+	if entry.SecretType == "" {
+		entry.SecretType = secretType(flds["secret_type"].Default.(string))
+	}
+	if !entry.Root {
+		if entry.TTL == 0 {
+			entry.TTL = time.Hour
+		}
+	}
+}
