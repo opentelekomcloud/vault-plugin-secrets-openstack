a8bb11e84a8466939672d4255c65f5202ac2ace9
Anton Kachurin <anton.kachurin@t-systems.com>
2022-03-02 21:02:00 +0300

    Remove not working functional tests coverage args

--- scripts/acceptance.sh
+++ scripts/acceptance.sh
@@ -2,7 +2,7 @@
 vault server -dev -dev-root-token-id=root -dev-plugin-dir="$VAULT_PLUGIN_DIR" >/dev/null 2>&1 &
 pid=$!
 
-go test -count=1 -v -timeout=20m -covermode atomic -coverprofile coverage-func.out -coverpkg ./... ./acceptance
+go test -count=1 -v -timeout=20m ./acceptance
 rc=$?
 
 kill $pid
