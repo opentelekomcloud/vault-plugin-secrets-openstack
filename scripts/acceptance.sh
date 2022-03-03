0a5588c286022eea1230334931651c028f2d6acb
Anton Kachurin <anton.kachurin@t-systems.com>
2022-03-03 11:27:40 +0300

    Refactor makefile and acceptance test script (#33)
    Refactor makefile and acceptance test script
    
    Unexport OS_... env variables in make
    Cleanup makefile
    Readability changes to acceptance.sh
    
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>

--- scripts/acceptance.sh
+++ scripts/acceptance.sh
@@ -1,8 +1,9 @@
-vault server -dev -dev-root-token-id=root -dev-plugin-dir="$VAULT_PLUGIN_DIR" > /dev/null 2>&1 &
+#!/usr/bin/env bash
+vault server -dev -dev-root-token-id=root -dev-plugin-dir="$VAULT_PLUGIN_DIR" >/dev/null 2>&1 &
+pid=$!
 
-rc=0
-go test -v -count 1 -timeout 20m -covermode atomic -coverprofile coverage-func.out ./acceptance/... || rc=1
+go test -count=1 -v -timeout=20m ./acceptance
+rc=$?
 
-pkill vault
-
+kill $pid
 exit $rc
