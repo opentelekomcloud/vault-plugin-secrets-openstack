82987934a2f17fe2c7c0c7aa4df48e105113264c
Anton Kachurin <anton.kachurin@t-systems.com>
2022-03-03 11:23:15 +0300

    Add acceptance tests for `clouds`

---
+++ acceptance/cloud_test.go
@@ -0,0 +1,98 @@
+package acceptance
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/hashicorp/vault/sdk/helper/jsonutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func (p *PluginTest) TestCloudLifecycle() {
+	t := p.T()
+
+	cloudData := map[string]interface{}{
+		"auth_url":         "https://example.com/v3/",
+		"username":         "admin",
+		"password":         "RcigTiYrJjVmEkrV71Cd",
+		"user_domain_name": "Default",
+	}
+	cloudName := "test-write"
+
+	t.Run("WriteCloud", func(t *testing.T) {
+		resp, err := p.vaultDo(
+			http.MethodPost,
+			cloudURL(cloudName),
+			cloudData,
+		)
+		require.NoError(t, err)
+		assert.Equal(t, http.StatusNoContent, resp.StatusCode, readJSONResponse(resp))
+	})
+
+	t.Run("ReadCloud", func(t *testing.T) {
+		resp, err := p.vaultDo(
+			http.MethodGet,
+			cloudURL(cloudName),
+			nil,
+		)
+		require.NoError(t, err)
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+		json, err := jsonToMap(readJSONResponse(resp))
+		require.NoError(t, err)
+		assert.Equal(t, cloudData, json["data"])
+	})
+
+	t.Run("ListClouds", func(t *testing.T) {
+		resp, err := p.vaultDo("LIST", cloudsListURL, nil)
+		require.NoError(t, err)
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+		names := extractCloudNames(t, readJSONResponse(resp))
+		require.Len(t, names, 1)
+		assert.Equal(t, cloudName, names[0])
+	})
+
+	t.Run("DeleteCloud", func(t *testing.T) {
+		resp, err := p.vaultDo(
+			http.MethodDelete,
+			cloudURL(cloudName),
+			nil,
+		)
+		require.NoError(t, err)
+		assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+	})
+}
+
+func jsonToMap(src string) (map[string]interface{}, error) {
+	var result map[string]interface{}
+	err := jsonutil.DecodeJSON([]byte(src), &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, err
+}
+
+var cloudsListURL = "/v1/openstack/clouds"
+
+func cloudURL(name string) string {
+	return fmt.Sprintf("/v1/openstack/cloud/%s", name)
+}
+
+func extractCloudNames(t *testing.T, jsonResponse string) []string {
+	t.Helper()
+	res, err := jsonToMap(jsonResponse)
+	require.NoError(t, err)
+
+	data, ok := res["data"].(map[string]interface{})
+	require.True(t, ok)
+	keys, ok := data["keys"].([]interface{})
+	require.True(t, ok)
+	keysStr := make([]string, len(keys))
+	for i, v := range keys {
+		keysStr[i] = v.(string)
+	}
+	return keysStr
+}
