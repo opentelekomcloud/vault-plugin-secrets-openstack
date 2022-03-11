aff7c9910586469a21f4a8443a8386bae9b3b5ef
Anton Kachurin <anton.kachurin@t-systems.com>
2022-03-11 17:03:25 +0300

    Add check for cloud existence when creating role (#50)
    Add check for cloud existence when creating role
    
    Fix #43
    
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>

--- acceptance/roles_test.go
+++ acceptance/roles_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/gophercloud/gophercloud/acceptance/tools"
 	"github.com/hashicorp/vault/sdk/helper/jsonutil"
+	"github.com/opentelekomcloud/vault-plugin-secrets-openstack/openstack"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -40,7 +41,16 @@ func extractRoleData(t *testing.T, resp *http.Response) *roleData {
 func (p *PluginTest) TestRoleLifecycle() {
 	t := p.T()
 
-	data := expectedRoleData()
+	cloud := &openstack.OsCloud{
+		Name:           openstack.RandomString(openstack.NameDefaultSet, 10),
+		AuthURL:        "http://example.com/v3",
+		UserDomainName: openstack.RandomString(openstack.NameDefaultSet, 10),
+		Username:       openstack.RandomString(openstack.NameDefaultSet, 10),
+		Password:       openstack.RandomString(openstack.PwdDefaultSet, 10),
+	}
+	p.makeCloud(cloud)
+
+	data := expectedRoleData(cloud.Name)
 	roleName := "test-write"
 
 	t.Run("WriteRole", func(t *testing.T) {
@@ -99,9 +109,9 @@ func roleURL(name string) string {
 	return fmt.Sprintf("/v1/openstack/role/%s", name)
 }
 
-func expectedRoleData() map[string]interface{} {
+func expectedRoleData(cloudName string) map[string]interface{} {
 	expectedMap := map[string]interface{}{
-		"cloud":        tools.RandomString("cl", 5),
+		"cloud":        cloudName,
 		"ttl":          time.Hour / time.Second,
 		"project_id":   "",
 		"project_name": tools.RandomString("p", 5),
