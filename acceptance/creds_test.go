847b9d261da621959317acab3cb6de50aa81ec09
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2022-03-11 16:23:34 +0300

    Add acceptance test for `creds` path

---
+++ acceptance/creds_test.go
@@ -0,0 +1,82 @@
+//go:build acceptance
+// +build acceptance
+
+package acceptance
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/opentelekomcloud/vault-plugin-secrets-openstack/openstack"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func (p *PluginTest) TestCredsLifecycle() {
+	t := p.T()
+
+	cloud := openstackCloudConfig(t)
+	require.NotEmpty(t, cloud)
+
+	// create temporary user, so base root won't be rotated
+	newCloud := p.makeChildCloud(cloud)
+
+	t.Run("WriteCloud", func(t *testing.T) {
+		resp, err := p.vaultDo(
+			http.MethodPost,
+			cloudURL(cloudName),
+			cloudToCloudMap(newCloud),
+		)
+		require.NoError(t, err)
+		assert.Equal(t, http.StatusNoContent, resp.StatusCode, readJSONResponse(t, resp))
+	})
+
+	_, aux := openstackClient(t)
+	roleName := openstack.RandomString(openstack.NameDefaultSet, 4)
+
+	t.Run("WriteRole", func(t *testing.T) {
+		resp, err := p.vaultDo(
+			http.MethodPost,
+			roleURL(roleName),
+			cloudToRoleMap(newCloud, aux),
+		)
+		require.NoError(t, err)
+		assert.Equal(t, http.StatusOK, resp.StatusCode, readJSONResponse(t, resp))
+	})
+
+	t.Run("CredsRootToken", func(t *testing.T) {
+		resp, err := p.vaultDo(
+			http.MethodGet,
+			credsURL(roleName),
+			nil,
+		)
+		require.NoError(t, err)
+		assert.Equal(t, http.StatusOK, resp.StatusCode, readJSONResponse(t, resp))
+	})
+}
+
+func credsURL(roleName string) string {
+	return fmt.Sprintf("/v1/openstack/creds/%s", roleName)
+}
+
+func cloudToCloudMap(cloud *openstack.OsCloud) map[string]interface{} {
+	return map[string]interface{}{
+		"name":             cloud.Name,
+		"auth_url":         cloud.AuthURL,
+		"username":         cloud.Username,
+		"password":         cloud.Password,
+		"user_domain_name": cloud.UserDomainName,
+	}
+}
+
+func cloudToRoleMap(cloud *openstack.OsCloud, auxData *AuxiliaryData) map[string]interface{} {
+	return map[string]interface{}{
+		"cloud":       cloud.Name,
+		"ttl":         time.Hour / time.Second,
+		"project_id":  auxData.ProjectID,
+		"root":        true,
+		"secret_type": "token",
+	}
+}
