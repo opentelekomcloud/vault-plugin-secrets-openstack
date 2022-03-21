c4636208353ad44f2d33197188b8ade8389cbddc
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2022-03-21 10:14:39 +0300

    Revert changes in sh script

--- scripts/acceptance.sh
+++ scripts/acceptance.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-vault server -dev -log-level=debug -dev-root-token-id=root -dev-plugin-dir="$VAULT_PLUGIN_DIR" >./output.log 2>&1 &
+vault server -dev -dev-root-token-id=root -dev-plugin-dir="$VAULT_PLUGIN_DIR" >/dev/null 2>&1 &
 pid=$!
 
-go test -count=1 -v -timeout=20m -tags=acceptance -parallel=1 ./acceptance
+go test -count=1 -v -timeout=20m -tags=acceptance ./acceptance
 rc=$?
 
 kill $pid
