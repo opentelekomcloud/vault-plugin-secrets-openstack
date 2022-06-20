700897c15286be4881f4bdcb48da23fe573bf972
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2022-06-20 12:15:08 +0300

    Reinit Client

--- openstack/backend.go
+++ openstack/backend.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
-	"github.com/gophercloud/gophercloud/openstack/identity/v3/tokens"
 	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
 )
@@ -86,21 +85,8 @@ func (c *sharedCloud) getClient(ctx context.Context, s logical.Storage) (*gopher
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	if c.client != nil {
-		valid, err := tokens.Validate(c.client, c.client.Token())
-		if err != nil {
-			return nil, err
-		}
-
-		if !valid {
-			if err := c.initClient(ctx, s); err != nil {
-				return nil, err
-			}
-		}
-	} else {
-		if err := c.initClient(ctx, s); err != nil {
-			return nil, err
-		}
+	if err := c.initClient(ctx, s); err != nil {
+		return nil, err
 	}
 
 	return c.client, nil
