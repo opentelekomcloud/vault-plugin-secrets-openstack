a6024238798b0c199091e552853f28758fb9ac5f
artem_lifshits <artem.lifshits@t-systems.com>
2023-02-09 12:15:40 +0300

    Clouds: autorotation implementation

--- openstack/backend.go
+++ openstack/backend.go
@@ -3,7 +3,10 @@ package openstack
 import (
 	"context"
 	"fmt"
+	"github.com/gophercloud/gophercloud/openstack/identity/v3/users"
+	"github.com/hashicorp/go-multierror"
 	"github.com/opentelekomcloud/vault-plugin-secrets-openstack/openstack/common"
+	"net/http"
 	"sync"
 	"time"
 
@@ -32,8 +35,8 @@ type sharedCloud struct {
 
 type backend struct {
 	*framework.Backend
-
-	clouds map[string]*sharedCloud
+	clouds               map[string]*sharedCloud
+	checkAutoRotateAfter time.Time
 }
 
 func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend, error) {
@@ -62,7 +65,8 @@ func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend,
 			secretToken(b),
 			secretUser(b),
 		},
-		BackendType: logical.TypeLogical,
+		BackendType:  logical.TypeLogical,
+		PeriodicFunc: b.periodicFunc,
 	}
 
 	if err := b.Setup(ctx, conf); err != nil {
@@ -145,5 +149,84 @@ func (c *sharedCloud) initClient(ctx context.Context, s logical.Storage) error {
 	c.expiresAt = token.ExpiresAt
 	c.client = sClient
 
+	return nil
+}
+
+func (b *backend) periodicFunc(ctx context.Context, req *logical.Request) error {
+	// Check for autorotation once an hour to avoid unnecessarily iterating
+	// over all keys too frequently.
+	if time.Now().Before(b.checkAutoRotateAfter) {
+		return nil
+	}
+	b.Logger().Debug("periodic func", "rotate-root", "rotation cycle in progress")
+	b.checkAutoRotateAfter = time.Now().Add(1 * time.Hour)
+
+	return b.autoRotateKeys(ctx, req)
+}
+
+func (b *backend) autoRotateKeys(ctx context.Context, req *logical.Request) error {
+	keys, err := req.Storage.List(ctx, "clouds/")
+	if err != nil {
+		return err
+	}
+
+	// Collect errors in a multierror to ensure a single failure doesn't prevent
+	// all keys from being rotated.
+	var errs *multierror.Error
+
+	for _, key := range keys {
+		cloudEntry := b.getSharedCloud(key)
+		if cloudEntry == nil {
+			continue
+		}
+
+		err = b.rotateIfRequired(ctx, req, cloudEntry)
+		if err != nil {
+			errs = multierror.Append(errs, err)
+		}
+	}
+	b.Logger().Debug("periodic func", "rotate-root", "rotation cycle complete")
+	return errs.ErrorOrNil()
+}
+
+func (b *backend) rotateIfRequired(ctx context.Context, req *logical.Request, sCloud *sharedCloud) error {
+	cloudConfig, err := sCloud.getCloudConfig(ctx, req.Storage)
+	if err != nil {
+		return err
+	}
+	if time.Now().After(cloudConfig.RootPasswordExpirationDate) {
+		client, err := sCloud.getClient(ctx, req.Storage)
+		if err != nil {
+			return logical.CodedError(http.StatusConflict, common.LogHttpError(err).Error())
+		}
+		newPassword, err := sCloud.passwords.Generate(ctx)
+		if err != nil {
+			return err
+		}
+
+		// make sure we don't use this cloud until the password is changed
+		sCloud.lock.Lock()
+		defer sCloud.lock.Unlock()
+
+		user, err := tokens.Get(client, client.Token()).ExtractUser()
+		if err != nil {
+			return logical.CodedError(http.StatusConflict, common.LogHttpError(err).Error())
+		}
+		err = users.ChangePassword(client, user.ID, users.ChangePasswordOpts{
+			Password:         newPassword,
+			OriginalPassword: cloudConfig.Password,
+		}).ExtractErr()
+		if err != nil {
+			errorMessage := fmt.Sprintf("error changing root password: %s", common.LogHttpError(err).Error())
+			return logical.CodedError(http.StatusConflict, errorMessage)
+		}
+		cloudConfig.Password = newPassword
+		cloudConfig.RootPasswordExpirationDate = time.Now().Add(cloudConfig.RootPasswordTTL)
+
+		if err := cloudConfig.save(ctx, req.Storage); err != nil {
+			return err
+		}
+		b.Logger().Debug("password rotated", "cloud", cloudConfig.Name)
+	}
 	return nil
 }
